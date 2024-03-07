84e77c19404ddd2e870c44d6e89f83b70cf4cb59
Sassoon Kuyumcian <sass.kumuycian@tabcorp.com.au>
2024-03-07 17:27:22 +1100

    add fugitive

--- .config/nvim/lua/sassy/plugins/gitsigns.lua
+++ .config/nvim/lua/sassy/plugins/gitsigns.lua
@@ -1,5 +1,13 @@
 return {
   "lewis6991/gitsigns.nvim",
   event = { "BufReadPre", "BufNewFile" },
-  config = true -- runs require('gitsigns').setup()
+  config = function()
+    local gitsigns = require("gitsigns")
+
+    gitsigns.setup()
+
+    local keymap = vim.keymap 
+    keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>")
+    keymap.set("n", "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>")
+  end,
 }
